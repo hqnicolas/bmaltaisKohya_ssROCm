6725c634f26d41f7a35cb8887d9022ce1658f440
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-11-03 16:45:56 -0300

    v24.1.7

--- drivers-amd.sh
+++ drivers-amd.sh
@@ -8,6 +8,7 @@ sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
 sudo usermod -a -G render,video $LOGNAME
 wget https://repo.radeon.com/amdgpu-install/6.2.2/ubuntu/jammy/amdgpu-install_6.2.60202-1_all.deb
 sudo apt install ./amdgpu-install_6.2.60202-1_all.deb -y
-sudo apt-get update
+sudo apt-get update -y
+sudo apt install gcc-12 -y
 sudo apt install amdgpu-dkms -y
 sudo apt install rocm -y
