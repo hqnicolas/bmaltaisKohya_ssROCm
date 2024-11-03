2c0867d932ddf8ebd3a664e63c8fd8b5fd56d479
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-11-03 14:46:25 -0300

    v24.1.7

--- drivers-amd.sh
+++ drivers-amd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt update -y && sudo apt full-upgrade -y
+sudo apt update -y
 sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
 sudo usermod -a -G render,video $LOGNAME
 # to radeon RX6000: wget https://repo.radeon.com/amdgpu-install/6.0.2/ubuntu/jammy/amdgpu-install_6.0.60002-1_all.deb
