b65b9070c9efd1e5092abd560b98f5e11dcf06e6
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-11-03 12:31:09 -0300

    v24.1.7

--- drivers-amd.sh
+++ drivers-amd.sh
@@ -7,5 +7,6 @@ wget https://repo.radeon.com/amdgpu-install/6.1.4/ubuntu/jammy/amdgpu-install_6.
 # to Ubuntu 20.04: wget https://repo.radeon.com/amdgpu-install/6.0.2/ubuntu/focal/amdgpu-install_6.0.60002-1_all.deb
 # to radeon RX6000: sudo apt install ./amdgpu-install_6.0.60002-1_all.deb -y
 sudo apt install ./amdgpu-install_6.1.60104-1_all.deb -y
+sudo apt-get update
 sudo apt install amdgpu-dkms -y
 sudo apt install rocm -y
