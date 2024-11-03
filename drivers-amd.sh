15da374caabc7dec845b28ca1a309ccd61cd3076
Nicolas Pereira <41456803+hqnicolas@users.noreply.github.com>
2024-11-03 15:42:34 -0300

    v24.1.7

--- drivers-amd.sh
+++ drivers-amd.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-sudo apt update && sudo apt full-upgrade -y
-sudo apt remove linux-image-6.5.0-18-generic
-
+sudo apt update -y
+# taken from https://rocm.docs.amd.com/projects/install-on-linux/en/latest/install/quick-start.html
 sudo apt install "linux-headers-$(uname -r)" "linux-modules-extra-$(uname -r)"
 sudo usermod -a -G render,video $LOGNAME
-# to radeon RX6000: wget https://repo.radeon.com/amdgpu-install/6.0.2/ubuntu/jammy/amdgpu-install_6.0.60002-1_all.deb
-wget https://repo.radeon.com/amdgpu-install/6.1.4/ubuntu/jammy/amdgpu-install_6.1.60104-1_all.deb
-# to Ubuntu 20.04: wget https://repo.radeon.com/amdgpu-install/6.0.2/ubuntu/focal/amdgpu-install_6.0.60002-1_all.deb
-# to radeon RX6000: sudo apt install ./amdgpu-install_6.0.60002-1_all.deb -y
-sudo apt install ./amdgpu-install_6.1.60104-1_all.deb -y
+wget https://repo.radeon.com/amdgpu-install/6.2.2/ubuntu/jammy/amdgpu-install_6.2.60202-1_all.deb
+sudo apt install ./amdgpu-install_6.2.60202-1_all.deb -y
 sudo apt-get update
 sudo apt install amdgpu-dkms -y
 sudo apt install rocm -y
